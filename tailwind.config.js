6b27267908632a713593eeaa18791b097a293157
Jorge Cruz <j.alexcruz@gmail.com>
2025-02-25 12:20:13 -0300

    Add custom fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Primary: ["Raleway", "sans-serif"],
+        Secondary: ["Nunito", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
